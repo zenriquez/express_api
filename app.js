24f446e39c70f07b623c7a4ca9934dcb3ff3d282
zenriquez <zenriquez@uabc.edu.mx>
2022-05-13 10:39:42 -0500

    Second GET working, api getting ID and Name

--- app.js
+++ app.js
@@ -18,6 +18,14 @@ app.get('/v1/explorers', (req, res) => {
 
 })
 
+//Second GET
+app.get('/v1/explorers/:id', (req, res) => {
+    console.log(`Api Explorers GET request ${new Date()}`)
+    console.log(`Getting explorer with id ${req.params.id}`)
+    const explorer= {id: 1, name: "Zabdi"}
+    res.status(200).json(explorer)
+})
+
 // this starts the app
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
